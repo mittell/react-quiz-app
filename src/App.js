43824d51391efb8d9c4f408fcb5be5019909459d
Chris Mittell <mittell@hotmail.co.uk>
2022-08-26 13:57:25 +0900

    feat: added mui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import FinalScore from './pages/FinalScore';
 import Questions from './pages/Questions';
 import Settings from './pages/Settings';
 
+import { Container, Box } from '@mui/material';
+
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Settings />} />
-				<Route path='/questions' element={<Questions />} />
-				<Route path='/score' element={<FinalScore />} />
-				<Route path='*' element={<Navigate to='/' replace />} />
-			</Routes>
+			<Container maxWidth='sm'>
+				<Box textAlign='center' mt={5}>
+					<Routes>
+						<Route path='/' element={<Settings />} />
+						<Route path='/questions' element={<Questions />} />
+						<Route path='/score' element={<FinalScore />} />
+						<Route path='*' element={<Navigate to='/' replace />} />
+					</Routes>
+				</Box>
+			</Container>
 		</BrowserRouter>
 	);
 }
