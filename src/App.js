c9067b105bc1be6e3d5fa3214883e68fa5316d01
Chris Mittell <mittell@hotmail.co.uk>
2022-08-26 13:46:12 +0900

    feat: added react-router-dom, setup routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import FinalScore from './pages/FinalScore';
+import Questions from './pages/Questions';
+import Settings from './pages/Settings';
+
 function App() {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Settings />} />
+				<Route path='/questions' element={<Questions />} />
+				<Route path='/score' element={<FinalScore />} />
+				<Route path='*' element={<Navigate to='/' replace />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
