fa9fba988b7ad4517288422a51c4027e42e0724e
Chris Mittell <mittell@hotmail.co.uk>
2022-08-26 14:14:05 +0900

    feat: setup base Settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+
+import { Typography, Box, Button } from '@mui/material';
 import SelectField from '../components/SelectField';
+import TextField from '../components/TextField';
 
 function Settings() {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<>
 			<Typography variant='h2' fontWeight='bold'>
 				Quiz App
 			</Typography>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<SelectField label='Category' />
 				<SelectField label='Difficulty' />
 				<SelectField label='Type' />
+
+				<TextField />
+
+				<Box mt={3} width='100%'>
+					<Button fullWidth variant='contained' type='submit'>
+						Get Started
+					</Button>
+				</Box>
 			</form>
 		</>
 	);
