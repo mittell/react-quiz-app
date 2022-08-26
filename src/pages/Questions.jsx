15305c8043c6c1e0a08df09055058757a01f78ae
Chris Mittell <mittell@hotmail.co.uk>
2022-08-26 14:59:58 +0900

    feat: setup base Questions page

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import { Typography, Box, Button } from '@mui/material';
+import useAxios from '../hooks/useAxios';
+
 function Questions() {
-	return <div>Questions</div>;
+	let apiUrl = `/api.php?amount=10`;
+
+	const { response, loading } = useAxios({ url: apiUrl });
+
+	return (
+		<Box>
+			<Typography variant='h4'>Question 1</Typography>
+			<Typography mt={5}>This is a question?</Typography>
+			<Box mt={2}>
+				<Button variant='contained'>Answer 1</Button>
+			</Box>
+			<Box mt={2}>
+				<Button variant='contained'>Answer 2</Button>
+			</Box>
+			<Box mt={5}>Score 2 / 6</Box>
+		</Box>
+	);
 }
 
 export default Questions;
